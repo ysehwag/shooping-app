1568f714065e16fe6b63fd73391775197346850a
Yogesh Kumar <yogesh.kumar@nagarro.com>
2019-04-15 12:37:06 +0530

    Configured server.js

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/',express.static( path.join(__dirname, 'public')))
 
 app.use('/',require('./Interfaces'))
-app.listen(8181)
+const PORT = process.env.PORT || 1443
+app.listen(PORT,()=>{
+    console.log('server is running at '+'http://localhost:'+PORT)
+})
